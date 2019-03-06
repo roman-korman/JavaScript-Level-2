1b8372e3da692865cd1e682477a74d227de37d4c
Roman <naydak@gmail.com>
2019-03-06 10:00:54 +0300

    Выполнил третье задание
    Сейчас после каждого товара на странице выводится запятая. Из-за чего это происходит? Как это исправить?

--- Lesson01/script.js
+++ Lesson01/script.js
@@ -16,11 +16,13 @@ const renderGoodsItem = (title, price)=>
 const renderGoodsList = list => {
   const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
   document.querySelector('.goods-list').innerHTML = goodsList.join('');
+  console.log(goodsList.join(''));
 }
 
 //Запятая выводится из за метода присоедниенеия контента
-//"= goodsList;" -выводится
-//"= goodsList.join('');"- не выводится
+//"= goodsList;" -выводится в виде массива, разделиетем в котором является запятые.
+//"= goodsList.join('');"- Метод join склеивает массив в строку используя в качестве разделителя аргумент.
+//Передавая '' в качестве аргумента аргумента мы заменяем его не "ничто"
 
 window.onload = () => {
   renderGoodsList(goods);
