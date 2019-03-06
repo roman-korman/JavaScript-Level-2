551a00a99b0ceb6efed18eb1dec41b20e5394b0e
Roman <naydak@gmail.com>
2019-03-06 11:18:14 +0300

    Сделал чать второго задания
    Добавьте значения по умолчанию для аргументов функции.

--- Lesson01/script.js
+++ Lesson01/script.js
@@ -1,17 +1,22 @@
 const goods = [
   { title: 'Shirt', price: 150 },
+  { title: 'Shirt2'},
   { title: 'Socks', price: 50 },
+  { price: 80 },
   { title: 'Jacket', price: 350 },
   { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title, price)=> 
-  `<div class="goods-item">
+const renderGoodsItem = (title, price)=> {
+  title = title || 'no title';
+  price = price || 'no price';
+  return `<div class="goods-item">
     <img src="" alt="">
     <h3>${title}</h3>
     <p>Цена: ${price}</p>
     <button>Добавить</button>
   </div>`;
+}
 
 const renderGoodsList = list => {
   const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
