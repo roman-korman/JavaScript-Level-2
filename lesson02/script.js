8304154e22a1ceb877a7c3de976613a477ebd73c
Roman <naydak@gmail.com>
2019-03-11 08:46:27 +0300

    Код переделан на классы

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,28 +1,63 @@
 'use strict';
 
-const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Shirt2'},
-  { title: 'Socks', price: 50 },
-  { price: 80 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
-];
+/**
+ * Объект одного товара, с возможностью вывода информации в виде HTML кода
+ */
+class GoodItem {
+	/**
+	 * Конструктора
+	 * @param {Наименование товара с проверкой} title 
+	 * @param {Цена товара с проверкой} price 
+	 */
+	constructor(title = 'no title', price = 'no price') {
+	  this.title = title;
+	  this.price = price;
+	}
+	render() {
+	  return `<div class="goods-item">
+	  <img src="" alt="">
+	  <h3>${this.title}</h3>
+	  <p>Цена: ${this.price}</p>
+	  <button>Добавить</button>
+		</div>`;
+	}
+  }
+/**
+ * Объект содержащий список товаров и умеющий выводить на страницу их используя объект GoodItem
+ */
+class GoodsList {
+	constructor() {
+	  this.goods = []
+	}
+	fetchGoods() {
+	  this.goods = [
+		{ title: 'Shirt', price: 150 },
+		{ title: 'Shirt2'},
+		{ title: 'Socks', price: 50 },
+		{ price: 80 },
+		{ title: 'Jacket', price: 350 },
+		{ title: 'Shoes', price: 250 }
+	  ]
+	}
+	render() {
+	  let listHtml = '';
+	  this.goods.forEach((good) => {
+		const goodItem = new GoodItem(good.title, good.price)
+		listHtml += goodItem.render()
+	  })
+	  document.querySelector('.goods-list').innerHTML = listHtml
+	}
+  }
+  
+/**
+ * Создаю экземпляр класса GoodsList, вызваю для него метод fetchGoods, чтобы записать список товаров в свойство goods
+ */
+const list = new GoodsList();
+list.fetchGoods();
 
-const renderGoodsItem = (title = 'no title', price = 'no price')=> 
-  `<div class="goods-item">
-    <img src="" alt="">
-    <h3>${title}</h3>
-    <p>Цена: ${price}</p>
-    <button>Добавить</button>
-  </div>`;
-
-
-const renderGoodsList = list => {
-  const goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-  document.querySelector('.goods-list').innerHTML = goodsList.join('');
-}
-
+/**
+ * вызваю render() метода fetchGoods
+ */
 window.onload = () => {
-  renderGoodsList(goods);
+  list.render()
 };
